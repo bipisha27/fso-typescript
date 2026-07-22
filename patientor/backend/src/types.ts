36b6fdfe8ca55285eaebf5ac6ad218575656059c
Bipisha <paudelbipisha4@gmail.com>
2026-07-22 12:40:39 +0545

    fixed lint errors

--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -1,5 +1,4 @@
 import { z } from "zod";
-import { describe } from "zod/v4/core";
 
 export const Gender = {
   Male: "male",
--- patientor/backend/src/utils.ts
+++ patientor/backend/src/utils.ts
@@ -1,4 +1,3 @@
-import { unknown } from "zod";
 import {
   NewPatientEntry,
   NewPatientSchema,
